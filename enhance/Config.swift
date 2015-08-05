8fd395ba7e571f82cd7caf7a2657a11f36b452fb
Jeff Hodsdon <jeffhodsdon@gmail.com>
2015-08-05 15:52:19 -0400

    Init

---
+++ enhance/Config.swift
@@ -0,0 +1,15 @@
+//
+//  Config.swift
+//  enhance
+//
+//  Created by Jeff Hodsdon on 8/5/15.
+//  Copyright (c) 2015 Jeff Hodsdon. All rights reserved.
+//
+
+import UIKit
+
+let kScreenWidth = UIScreen.mainScreen().bounds.width
+let kScreenHeight = UIScreen.mainScreen().bounds.height
+
+let kEnhanceTealColor = UIColor(red: 80.0/255.0, green: 227.0/255.0, blue: 194.0/255.0, alpha: 1.0)
+let kEnhanceDarkTealColor = UIColor(red: 39.0/255.0, green: 50.0/255.0, blue: 48.0/255.0, alpha: 1.0)
