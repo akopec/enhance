43ede5be30ff5a165e99a494fcae3177130dfc91
Jonathan Baker <jonathan@jonathanbaker.me>
2017-04-22 08:00:12 -0400

    Add Unimplemented function.

---
+++ Enhance/Unimplemented.swift
@@ -0,0 +1,11 @@
+//
+//  Unimplemented.swift
+//  Enhance
+//
+//  Created by Jonathan Baker on 4/22/17.
+//
+//
+
+func unimplemented(_ function: String = #function) -> Never  {
+    fatalError("\(function) is not implemented.")
+}
