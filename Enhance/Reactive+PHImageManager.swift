ea1bde2e0df51bbdb280b1d6f9fb5a1f55370975
Jonathan Baker <jonathan@jonathanbaker.me>
2017-04-23 13:58:08 -0400

    Add PHImageManager reactive extensions.

---
+++ Enhance/Reactive+PHImageManager.swift
@@ -0,0 +1,33 @@
+//
+//  Reactive+PHImageManager.swift
+//  Enhance
+//
+//  Created by Jonathan Baker on 4/23/17.
+//
+//
+
+import Photos
+import Result
+import ReactiveSwift
+
+extension Reactive where Base: PHImageManager {
+    func requestImage(for asset: PHAsset, targetSize: CGSize, contentMode: PHImageContentMode, options: PHImageRequestOptions? = nil) -> SignalProducer<UIImage, AnyError> {
+        return SignalProducer({ observer, disposable in
+            let id = self.base.requestImage(for: asset, targetSize: targetSize, contentMode: contentMode, options: options, resultHandler: { image, info in
+                if let image = image {
+                    observer.send(value: image)
+                    if !(info?[PHImageResultIsDegradedKey] as? Bool ?? false) {
+                        observer.sendCompleted()
+                    }
+                }
+                else if let error = info?[PHImageErrorKey] as? NSError {
+                    observer.send(error: AnyError(error))
+                }
+            })
+
+            disposable.add({
+                self.base.cancelImageRequest(id)
+            })
+        })
+    }
+}
